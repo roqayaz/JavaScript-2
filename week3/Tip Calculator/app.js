6e6f0e6fa682275b0f6792582cbc447cb8e47586
roq zob <roqzob@gmail.com>
2021-09-11 17:44:07 +0430

    solved the each problem and add comment

--- week3/Tip Calculator/app.js
+++ week3/Tip Calculator/app.js
@@ -1,3 +1,5 @@
+
+// Grab the element
 const billAmount = document.getElementById('bill');
 const people = document.getElementById('people');
 const each = document.getElementById('each');
@@ -6,8 +8,10 @@ const button = document.getElementById('button');
 const service = document.getElementById('service');
 const total = document.getElementById('total');
 
-
+//The calculate function
 const calculate = () => {
+
+    //The alert box with the empty value
     if (billAmount.value === "" || service.value == 0 || people.value === "") {
         Swal.fire({
             icon: 'warning',
@@ -19,8 +23,8 @@ const calculate = () => {
           })
         return;
       };
-
-      if (billAmount.value <= 0 || people.value <= 0) {
+    // The alert box with the value under 1
+      if (billAmount.value <= 0) {
         Swal.fire({
             icon: 'warning',
             text: 'You have to put the right value!',
@@ -31,15 +35,16 @@ const calculate = () => {
           })
         return;   
       }
-
-      if (people.value = 1) {
-        
+      
+    //The each text appear when the number of people is more than 1
+      if (people.value <= 1) {
+        people.value = 1;
         each.style.display = "none";
       } else {
         each.style.display = "block";
       };
 
-
+    // The calculate method 
    let cal = (billAmount.value * service.value) / people.value;
 
    cal = Math.round(cal * 100) / 100;
@@ -48,4 +53,6 @@ const calculate = () => {
    total.style.display = "block";
    
 };
+
+//Call the function 
 button.addEventListener('click', calculate);
